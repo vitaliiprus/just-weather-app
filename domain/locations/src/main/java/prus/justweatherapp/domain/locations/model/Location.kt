c417facade03b5ca09b0fbb2195a41d2d90c1d41
vitaliyprus <dev.prus@gmail.com>
2024-02-03 11:10:47 +0300

    feat(domain): add Location model

---
+++ domain/locations/src/main/java/prus/justweatherapp/domain/locations/model/Location.kt
@@ -0,0 +1,12 @@
+package prus.justweatherapp.domain.locations.model
+
+data class Location(
+    val id: String,
+    val city: String,
+    val adminName: String? = null,
+    val country: String? = null,
+    val displayName: String? = null,
+    val orderIndex: Int? = null,
+    val lng: Double,
+    val lat: Double,
+)
