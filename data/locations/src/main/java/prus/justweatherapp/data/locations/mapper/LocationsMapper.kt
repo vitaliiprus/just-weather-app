1602cefbb70e14af1f2351c91437d07f0d3a704e
vitaliyprus <dev.prus@gmail.com>
2024-02-03 11:12:34 +0300

    feat(data): add Locations mapper from db to domain model

---
+++ data/locations/src/main/java/prus/justweatherapp/data/locations/mapper/LocationsMapper.kt
@@ -0,0 +1,19 @@
+package prus.justweatherapp.data.locations.mapper
+
+import prus.justweatherapp.domain.locations.model.Location
+import prus.justweatherapp.local.db.entity.LocationEntity
+
+internal fun LocationEntity.mapToDomainModel() =
+    Location(
+        id = this.locationId,
+        city = this.city,
+        adminName = this.adminName,
+        country = this.country,
+        lng = this.lng,
+        lat = this.lat
+    )
+
+internal fun List<LocationEntity>.mapToDomainModels() =
+    map {
+        it.mapToDomainModel()
+    }
