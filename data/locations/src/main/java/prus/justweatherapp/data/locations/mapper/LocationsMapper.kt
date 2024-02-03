c45ddcdfd240d1bbca827384f4bd483e93f8859c
vitaliyprus <dev.prus@gmail.com>
2024-02-03 11:47:48 +0300

    feat(data): add UserLocations mapper

--- data/locations/src/main/java/prus/justweatherapp/data/locations/mapper/LocationsMapper.kt
+++ data/locations/src/main/java/prus/justweatherapp/data/locations/mapper/LocationsMapper.kt
@@ -2,6 +2,8 @@ package prus.justweatherapp.data.locations.mapper
 
 import prus.justweatherapp.domain.locations.model.Location
 import prus.justweatherapp.local.db.entity.LocationEntity
+import prus.justweatherapp.local.db.entity.UserLocationEntity
+import prus.justweatherapp.local.db.model.UserLocationDbModel
 
 internal fun LocationEntity.mapToDomainModel() =
     Location(
@@ -17,3 +19,26 @@ internal fun List<LocationEntity>.mapToDomainModels() =
     map {
         it.mapToDomainModel()
     }
+internal fun UserLocationDbModel.mapToDomainModel() =
+    Location(
+        id = this.locationId,
+        city = this.city,
+        adminName = this.adminName,
+        displayName = this.displayName,
+        orderIndex = this.orderIndex,
+        country = this.country,
+        lng = this.lng,
+        lat = this.lat
+    )
+
+internal fun List<UserLocationDbModel>.mapToDomainModels() =
+    map {
+        it.mapToDomainModel()
+    }
+
+internal fun Location.toDbEntity() =
+    UserLocationEntity(
+        locationId = this.id,
+        displayName = this.displayName,
+        orderIndex = this.orderIndex!!
+    )
