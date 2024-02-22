2d3a4dc0d5c3ceccd513db574b96cbcc2121ef18
vitaliyprus <dev.prus@gmail.com>
2024-02-22 15:32:44 +0300

    feat(core): add JwaOutlinedButton component

---
+++ core/ui/src/main/kotlin/prus/justweatherapp/core/ui/components/JwaOutlinedButton.kt
@@ -0,0 +1,67 @@
+package prus.justweatherapp.core.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.unit.dp
+import prus.justweatherapp.core.ui.preview.parameterprovider.BooleanPreviewParameterProvider
+import prus.justweatherapp.theme.AppTheme
+import prus.justweatherapp.theme.accent
+import prus.justweatherapp.theme.textButtonStyle
+
+@Composable
+fun JwaOutlinedButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    enabled: Boolean = true,
+    onClick: () -> Unit = {}
+) {
+
+    OutlinedButton(
+        modifier = modifier,
+        shape = MaterialTheme.shapes.small,
+        colors = ButtonColors(
+            containerColor = Color.Transparent,
+            contentColor = accent,
+            disabledContainerColor = Color.Transparent,
+            disabledContentColor = accent.copy(alpha = 0.9f)
+        ),
+        border = BorderStroke(
+            width = 1.dp,
+            color = accent
+        ),
+        enabled = enabled,
+        onClick = onClick
+    ) {
+
+        Text(
+            text = text,
+            style = textButtonStyle,
+        )
+
+    }
+
+}
+
+@PreviewLightDark
+@Composable
+private fun JwaOutlinedButtonPreview(
+    @PreviewParameter(BooleanPreviewParameterProvider::class) enabled: Boolean
+) {
+    AppTheme {
+        Surface {
+            JwaOutlinedButton(
+                text = "Button",
+                enabled = enabled
+            )
+        }
+    }
+}
