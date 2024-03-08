e41ef5f6688a27180fe2ee1a7c5ebc91ee7da5c1
vitaliyprus <dev.prus@gmail.com>
2024-03-08 13:48:06 +0300

    feat(db): change db converters to work with LocalDateTime

--- local/db/src/main/kotlin/prus/justweatherapp/local/db/Converters.kt
+++ local/db/src/main/kotlin/prus/justweatherapp/local/db/Converters.kt
@@ -1,13 +1,19 @@
 package prus.justweatherapp.local.db
 
 import androidx.room.TypeConverter
-import java.util.Calendar
+import kotlinx.datetime.Instant
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toInstant
+import kotlinx.datetime.toLocalDateTime
 
 class Converters {
     @TypeConverter
-    fun calendarToDatestamp(calendar: Calendar): Long = calendar.timeInMillis
+    fun localDateTimeToDatestamp(dateTime: LocalDateTime): Long {
+        return dateTime.toInstant(TimeZone.UTC).toEpochMilliseconds() / 1000
+    }
 
     @TypeConverter
-    fun datestampToCalendar(value: Long): Calendar =
-        Calendar.getInstance().apply { timeInMillis = value }
+    fun datestampToLocalDateTime(value: Long): LocalDateTime =
+        Instant.fromEpochSeconds(value).toLocalDateTime(TimeZone.UTC)
 }
